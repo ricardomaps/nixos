2ab5e054d3d5e24ed0812d4880ed776a4adce745
ricmaps <ricardomapurungajunior@gmail.com>
2026-02-26 16:21:23 -0300

    started using qbittorrent as a headless service

--- modules/movie-piracy.nix
+++ modules/movie-piracy.nix
@@ -4,9 +4,20 @@
   {
     services.qbittorrent = {
       enable = true;
+      serverConfig = {
+        LegalNotice.Accepted = true;
+        Preferences = {
+          WebUI = {
+            LocalHostAuth = true;
+            Username = "ricmaps";
+            Password_PBKDF2 = "@ByteArray(ARQ77eY1NUZaQsuDHbIMCA==:0WMRkYTUWVT9wVvdDtHAjU9b3b7uB8NR1Gur2hmQCvCDpm39Q+PsJRJPaCU51dEiz+dTzh8qbPsL8WkFljQYFQ==)";
+          };
+          General.Locale = "en";
+        };
+      };
     };
 
-    environment.systemPackages = [ pkgs.vlc ];
+    environment.systemPackages = with pkgs; [ vlc ];
     
   };
 }
