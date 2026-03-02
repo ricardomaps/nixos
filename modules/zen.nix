d3859fb27104ba4b5b7cb3bb25cf8b3f29ab84ca
ricmaps <ricardomapurungajunior@gmail.com>
2026-03-01 22:34:10 -0300

    added settings for workspaces and pins for zen browser

--- modules/zen.nix
+++ modules/zen.nix
@@ -19,9 +19,9 @@
       inputs.zen-browser.homeModules.default
     ];
 
-    programs.zen-browser = {
+    programs.zen-browser =  {
       enable = true;
-      profiles.ricmaps = {
+      profiles.ricmaps = rec {
         name = "ricmaps";
         extensions = {
           force = true;
@@ -52,6 +52,14 @@
               definedAliases = ["@nx"];
             };
 
+            nixpkgs = {
+              name = "NixOS Search";
+              urls = lib.singleton {
+                template = "https://search.nixos.org/packages?channel=unstable&query={searchTerms}";
+              };
+              definedAliases = ["@np"];
+            };
+
             rustdocs = {
               name = "Docs.rs";
               urls = lib.singleton {
@@ -59,6 +67,53 @@
               };
               definedAliases = ["@rs"];
             };
+          };
+        };
+        spacesForce = true;
+        spaces = {
+          "Doom" = {
+            id = "c258e389-9a7b-4d75-bfe0-408ab1b92219";
+            position = 2000;
+          };
+          "Research" = {
+            id = "44fd5416-49a7-4645-88bf-6776b3319129";
+            position = 3000;
+          };
+        };
+
+        pinsForce = true;
+        pins = {
+          "ChatGPT" = {
+            id = "22b18904-c0c2-4122-9732-e54635c258e0";
+            url = "https://chatgpt.com";
+            workspace = spaces.Research.id;
+          };
+          "Claude" = {
+            id = "44fd5416-49a7-4645-88bf-6776b3319129";
+            url = "https://claude.ai/";
+            workspace = spaces.Research.id;
+          };
+          "Bluesky" = {
+            id = "2986bec7-4461-451e-b3b0-5c8df67f1110";
+            url = "https://bsky.app/";
+            workspace = spaces.Doom.id;
+          };
+          "Discord" = {
+            id = "e8503f35-9e3e-49a3-8a7b-02ecb7e559ed";
+            url = "https://discord.com";
+            workspace = spaces.Doom.id;
+        
+          };
+          "Reddit" = {
+            id = "a35d0a6a-b5a7-4627-a86a-cb520003b889";
+            url = "https://reddit.com";
+            workspace = spaces.Doom.id;
+          
+          };
+          "Youtube" = {
+            id = "d3ac5d6f-9acb-4598-aa35-a7c3debe0836";
+            url = "https://youtube.com";
+            workspace = spaces.Doom.id;
           };
         };
       };
